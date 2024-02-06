0fae518332bd03d3fb39502ea76ee753dbdbb3c0
VIvien-Parsis <vivienparsis.perso@gmail.com>
2024-02-06 17:38:31 +0100

    fix find method and, req2 and req3

--- requests/req3.py
+++ requests/req3.py
@@ -1,38 +1,9 @@
 from req_class import mongo_req
 
-# Requête pour afficher le total cumulé du nombre de cas confirmés, de décès, et de personnes guéries en France
-aggregate = [
-  {
-    "$match": {
-      "nom": "France",
-    },
-  },
-  {
-    "$group": {
-      "_id": "France",
-      "totalCasConfirme": {
-        "$sum": "$casConfirmes",
-      },
-      "totalDeces": {
-        "$sum": "$deces",
-      },
-      "totalGueris": {
-        "$sum": "$gueris",
-      },
-    },
-  },
-  {
-    "$project": {
-      "pays": "$_id",
-      "_id": 0,
-      "totalCasConfirme": 1,
-      "totalDeces": 1,
-      "totalGueris": 1,
-    },
-  },
-]
+# Obtenir le nombre de cas confirmé selon une région ou département et date choisis
+aggregate = []
 filePath = "./requests/req3_output.json"
 
-find, findProject = {},{}
+find, findProject = {"casConfirmes":{"$exists":"true"},"nom":"Île-de-France","date":"2020-03-24"},{"nom":1,"casConfirmes":1,"_id":0}
 req = mongo_req(aggregate,filePath,find,findProject)
-req.getOutputAggregate()
+req.getOutputFind()
--- requests/req_class.py
+++ requests/req_class.py
@@ -3,17 +3,17 @@ import pymongo
 
 class mongo_req:
     
-    def __init__(self, aggregate, filePath, find, findProject):
+    def __init__(self, aggregate, filePath, findMatch, findProject):
         self.aggregate = aggregate
         self.filePath = filePath
-        self.find = find
+        self.findMatch = findMatch
         self.findProject = findProject
-        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
-        self.db = self.client["nosql_project"]
-        self.col = self.db["covid"]
 
     def getOutputAggregate(self):
-        data = self.col.aggregate(self.aggregate)
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["nosql_project"]
+        col = db["covid"]
+        data = col.aggregate(self.aggregate)
         data_string = "["
         for item in data:
             data_string+=json.dumps(item)+",\n"
@@ -21,7 +21,16 @@ class mongo_req:
         data_string = data_string.replace(",\n]","]")
         with open(self.filePath, "w") as file:
             file.write(data_string)
+
     def getOutputFind(self):
-        data = self.col.find(self.find, self.findProject)
-        # with open(self.filePath, "w") as file:
-        #     file.write(data)
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["nosql_project"]
+        col = db["covid"]
+        data_string = "["
+        data = col.find(self.findMatch,self.findProject)
+        for item in data:
+            data_string+=json.dumps(item)+",\n"
+        data_string+="]"
+        data_string = data_string.replace(",\n]","]")
+        with open(self.filePath, "w") as file:
+            file.write(data_string)
