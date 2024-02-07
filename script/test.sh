4c1f91eb871859eab76f63581096fe9469f165e1
VIvien-Parsis <vivienparsis.perso@gmail.com>
2024-02-07 17:39:13 +0100

    fix test.sh

---
+++ script/test.sh
@@ -0,0 +1,20 @@
+echo "execute req1"
+python ./requests/req.py r=req1
+echo "execute req2"
+python ./requests/req.py r=req2
+echo "execute req3"
+python ./requests/req.py r=req3
+echo "execute req4"
+python ./requests/req.py r=req4
+echo "execute req5"
+python ./requests/req.py r=req5
+echo "execute req6"
+python ./requests/req.py r=req6
+echo "execute req7"
+python ./requests/req.py r=req7
+echo "execute req8"
+python ./requests/req.py r=req8
+echo "execute stat1"
+python ./stats/stat1.py
+echo "execute stat2"
+python ./stats/stat1.py
